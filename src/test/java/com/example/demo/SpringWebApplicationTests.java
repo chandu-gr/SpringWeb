97cd90a86211f6700cb9ade263b32bddbbfc148a
chandu-gr <55100458+chandu-gr@users.noreply.github.com>
2020-05-12 15:53:50 +0530

    Third commit
    Third commit

--- src/test/java/com/example/demo/SpringWebApplicationTests.java
+++ src/test/java/com/example/demo/SpringWebApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.controller.ProductController;
+import com.example.demo.service.ProductService;
 
 @SpringBootTest
 class SpringWebApplicationTests {
 
+	@Autowired
+	ProductService service;
+	@Autowired
+	ProductController controller;
+	
+	//unit test with Service class
 	@Test
-	void contextLoads() {
+	void testSelectAllProducts() {
+		assertEquals(9, service.selectAllProducts().size());
 	}
-
+	
+	//Integration test with Controller class
+	@Test
+	void testGetAllProducts() {
+		
+	}
+	
 }
